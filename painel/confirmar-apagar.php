2952d19b449184ced5206652821ad769449e026c
projetosstachowski <100374294+projetosstachowski@users.noreply.github.com>
2023-12-08 14:34:20 -0300

    Add files via upload

---
+++ painel/confirmar-apagar.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    if((!isset($_SESSION['id']) == true) and (!isset($_SESSION['nome']) == true) and (!isset($_SESSION['email']) == true)){
+        unset($_SESSION['id']);
+        unset($_SESSION['nome']);
+        unset($_SESSION['email']);
+        header('Location: ../index.html');
+    }
+    require('conecta.php');
+
+    $id_cliente = $_GET['id']; //Pega da URL
+
+    echo "ID==> $id_cliente";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        body {
+            padding-top: 50px;
+            padding-bottom: 20px;
+        }
+    </style>
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+    <h2>Tem certeza que quer apagar esse registro?</h2>
+    <a a href="apaga_cliente.php?id=<?php echo $id_cliente; ?>" class="btn btn-danger" >Apagar</a>
+    <a a href="index.php" class="btn btn-primary">Voltar para a página anterior</a>
+</body>
+</html>
